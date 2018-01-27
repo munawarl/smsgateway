03f637f504f74992a14cf485cdf0cb4fd825d189
munawarl <35635318+munawarl@users.noreply.github.com>
2018-01-27 18:53:21 -0500

    Update index.js
    Adding timer with 5 seconds for calling back Deskpro to load the message.

--- routes/index.js
+++ routes/index.js
@@ -92,7 +92,7 @@ router.post('/message', function(req, res, next) {
   if (ticketNumber == null || ticketNumber.length == 0 || message.indexOf(tmsgMark) < 0) {
   	callTwilioAPI(message, fromMobile, toMobile, req, res, next);
   } else {
-  	callDeskProAPI4TicketMsg(message, fromMobile, toMobile, req, res, next);
+  	setTimeout(callDeskProAPI4TicketMsg(message, fromMobile, toMobile, req, res, next), 5000);
   }
  
   
